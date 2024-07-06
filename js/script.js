b788c2c2e09e7fb78c89c2121af65ed2cd1c581e
mdshop268 <duduha20102@gmail.com>
2024-07-06 11:10:44 +0300

    dropdown_menu
    not completed

--- js/script.js
+++ js/script.js
@@ -1,5 +1,3 @@
-const openLink = window.open;
-
 window.addEventListener('load', () => {
     let footer = document.getElementById("footer");
     let year = new Date().getFullYear();
@@ -7,4 +5,26 @@ window.addEventListener('load', () => {
     footer.innerHTML = `&copy; ${year}. Усі права захищенні.`;
 });
 
+const openLink = window.open;
 
+document.addEventListener('DOMContentLoaded', (event) => {
+    const dropbtn = document.querySelector('.dropbtn');
+    const dropArrow = document.querySelector('.arrow');
+    const dropdownContent = document.querySelector('.dropdown-content');
+
+    dropbtn.addEventListener('click', function() {
+        if (dropdownContent.classList.contains('show')) {
+            dropArrow.classList.add("rotateL");
+            dropdownContent.classList.add('hide');
+            setTimeout(() => {
+                dropdownContent.classList.remove('show');
+                dropdownContent.classList.remove('hide');
+                dropArrow.classList.remove("rotateR");
+                dropArrow.classList.remove("rotateL");
+            }, 500);
+        } else {
+            dropdownContent.classList.add('show');
+            dropArrow.classList.add("rotateR");
+        }
+    });
+});
