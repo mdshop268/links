8379f350c6b74b62992b82701f67cd1f4bd607a0
mdshop268 <duduha20102@gmail.com>
2024-07-07 21:52:07 +0300

    update

--- js/script.js
+++ js/script.js
@@ -11,9 +11,31 @@ document.addEventListener('DOMContentLoaded', (event) => {
     const dropbtn = document.querySelector('.dropbtn');
     const arrow = document.querySelector('.arrow');
     const dropdownContent = document.querySelector('.dropdown-content');
+    const links = dropdownContent.querySelectorAll('.link');
 
     dropbtn.addEventListener('click', function() {
         arrow.classList.toggle("rotate");
-        dropdownContent.classList.toggle("show");
+
+        if (dropdownContent.classList.contains("show")) {
+            [...links].reverse().forEach((link, index) => {
+                setTimeout(() => {
+                    link.classList.remove("show");
+                    link.classList.add("hide");
+                }, index * 200); // Увеличиваем задержку между кнопками
+            });
+
+            setTimeout(() => {
+                dropdownContent.classList.remove("show");
+            }, links.length * 200);
+        } else {
+            dropdownContent.classList.add("show");
+
+            links.forEach((link, index) => {
+                setTimeout(() => {
+                    link.classList.add("show");
+                    link.classList.remove("hide");
+                }, index * 200); // Увеличиваем задержку между кнопками
+            });
+        }
     });
 });
